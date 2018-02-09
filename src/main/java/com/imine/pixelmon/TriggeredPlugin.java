bce1dbfaeb219f55a417cc3c65248a8313e944bb
Sansko1337 <sanderknauff@hotmail.com>
2018-02-09 01:02:49 +0100

    Inject config path

--- src/main/java/com/imine/pixelmon/TriggeredPlugin.java
+++ src/main/java/com/imine/pixelmon/TriggeredPlugin.java
@@ -3,26 +3,24 @@ package com.imine.pixelmon;
 import com.imine.pixelmon.event.TriggerEventListener;
 import com.imine.pixelmon.service.TriggerService;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
+import javax.inject.Inject;
 import java.nio.file.Path;
 
 @Plugin(id = "triggered", name = "Triggered", version = "1.0")
 public class TriggeredPlugin {
 
-    private final Path configPath;
-
-
-    public TriggeredPlugin(Path configPath) {
-        this.configPath = configPath.resolve("triggers.json");
-    }
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path configPath;
 
     @Listener
-    public void onServerStart() {
-        TriggerService triggerService = new TriggerService(configPath);
+    public void onServerStart(GameStartedServerEvent event) {
+        TriggerService triggerService = new TriggerService(configPath.resolve("triggers.json"));
         Sponge.getGame().getEventManager().registerListeners(this, new TriggerEventListener(triggerService));
-
-
     }
 }
