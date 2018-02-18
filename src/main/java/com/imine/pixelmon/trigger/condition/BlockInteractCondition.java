bf19b0d514ee4a932e2220823c309a42adfc8075
Dennis <Dennis.Mulder4@hva.nl>
2018-02-18 18:50:53 +0100

    fixed BlockInteractCondition, was not working because of my lazyness

--- src/main/java/com/imine/pixelmon/trigger/condition/BlockInteractCondition.java
+++ src/main/java/com/imine/pixelmon/trigger/condition/BlockInteractCondition.java
@@ -1,15 +1,35 @@
 package com.imine.pixelmon.trigger.condition;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.imine.pixelmon.trigger.requirement.Requirement;
 import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.world.Location;
 
 import java.util.List;
+import java.util.Optional;
 
-public abstract class BlockInteractCondition extends Condition {
+public class BlockInteractCondition extends Condition {
 
-    public BlockInteractCondition(List<Requirement> requirements) {
+    private int x, y, z;
+    private String world;
+
+    public BlockInteractCondition(@JsonProperty("requirements") List<Requirement> requirements, @JsonProperty("x") int x, @JsonProperty("y") int y, @JsonProperty("z") int z, @JsonProperty("world") String world) {
         super(requirements);
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.world = world;
     }
 
-    public abstract boolean blockMatches(BlockSnapshot block);
+    public boolean blockMatches(BlockSnapshot block) {
+        Optional<Location<org.spongepowered.api.world.World>> location = block.getLocation();
+        if (location.isPresent()) {
+            Location<org.spongepowered.api.world.World> worldLocation = location.get();
+            return this.x == worldLocation.getBlockX()
+                    && this.y == worldLocation.getBlockY()
+                    && this.z == worldLocation.getBlockZ()
+                    && this.world.equals(worldLocation.getExtent().getName());
+        }
+        return false;
+    }
 }
