efdfbdc033dd3c8bcd37ebc75ff14223530ed62f
Dennis <Dennis.Mulder4@hva.nl>
2018-06-01 23:13:51 +0200

    updated item revealer to only show items which you have not picked up yet

--- src/main/java/com/imine/pixelmon/component/ItemPickupRevealer.java
+++ src/main/java/com/imine/pixelmon/component/ItemPickupRevealer.java
@@ -1,58 +1,90 @@
 package com.imine.pixelmon.component;
 
 import com.flowpowered.math.vector.Vector3d;
+import com.imine.pixelmon.service.PlayerTriggerActivationService;
 import com.imine.pixelmon.service.TriggerService;
+import com.imine.pixelmon.trigger.Interval;
 import com.imine.pixelmon.trigger.Trigger;
-import com.imine.pixelmon.trigger.action.Action;
 import com.imine.pixelmon.trigger.action.GiveItemAction;
-import com.imine.pixelmon.trigger.condition.Condition;
 import com.imine.pixelmon.trigger.condition.IPositioned;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.effect.particle.ParticleEffect;
-import org.spongepowered.api.effect.particle.ParticleOption;
 import org.spongepowered.api.effect.particle.ParticleOptions;
 import org.spongepowered.api.effect.particle.ParticleTypes;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ItemPickupRevealer {
 
-    public static void init(Object plugin, TriggerService triggerService) {
+    public static void init(Object plugin, TriggerService triggerService, PlayerTriggerActivationService playerTriggerActivationService) {
         Sponge.getScheduler().createTaskBuilder()
-                .intervalTicks(5)
-                .execute(new Revealer(triggerService))
+                .intervalTicks(1)
+                .execute(new Revealer(triggerService, playerTriggerActivationService))
                 .submit(plugin);
     }
 
 
     private static class Revealer implements Runnable {
 
-        List<Location> locations;
+        private static final Logger LOGGER = LoggerFactory.getLogger(Revealer.class);
+        private Map<Trigger, Location> pickupLocations = new HashMap<>();
+        private PlayerTriggerActivationService playerTriggerActivationService;
 
-        public Revealer(TriggerService triggerService) {
-            this.locations = triggerService.getAll().stream()
+        public Revealer(TriggerService triggerService, PlayerTriggerActivationService playerTriggerActivationService) {
+            this.playerTriggerActivationService = playerTriggerActivationService;
+
+            triggerService.getAll()
+                    .stream()
                     .filter(trigger -> trigger.getActions().stream().anyMatch(GiveItemAction.class::isInstance))
-                    .flatMap(trigger -> trigger.getConditions().stream())
-                    .filter(IPositioned.class::isInstance)
-                    .map(IPositioned.class::cast)
-                    .map(IPositioned::getLocation)
-                    .collect(Collectors.toList());
+                    .peek(trigger -> LOGGER.info("Registering Trigger<{}> which has an item somewhere in the map.", trigger.getId()))
+                    .forEach(trigger -> trigger.getConditions()
+                            .stream()
+                            .filter(IPositioned.class::isInstance)
+                            .map(IPositioned.class::cast)
+                            .findFirst()
+                            .map(IPositioned::getLocation)
+                            .ifPresent(location -> pickupLocations.put(trigger, location))
+                    );
         }
 
         @Override
         public void run() {
-            locations.forEach(location -> {
-                ((World) location.getExtent()).spawnParticles(ParticleEffect.builder()
-                        .type(ParticleTypes.ENCHANTING_GLYPHS)
-                        .offset(new Vector3d(0.05,0.05,0.05))
-                        .velocity(new Vector3d(2,2,2))
-                        .quantity(5)
-                        .build(), location.getPosition().add(0.5F, 0.75F, 0.5F));
-            });
+
+            Sponge.getServer().getOnlinePlayers()
+                    .forEach(player -> pickupLocations.keySet().stream()
+                            .filter(trigger -> shouldShowForPlayer(trigger, player))
+                            .forEach(trigger ->
+                                    player.spawnParticles(
+                                            ParticleEffect.builder()
+                                                    .type(ParticleTypes.ENCHANTING_GLYPHS)
+                                                    .velocity(new Vector3d(0, 0.25, 0))
+                                                    .offset(new Vector3d(0.25, 0.25, 0.25))
+                                                    .option(ParticleOptions.OFFSET, new Vector3d(0, 0, 0))
+                                                    .quantity(5)
+                                                    .build(),
+                                            pickupLocations.get(trigger).getPosition()
+                                                    .add(0.5F, 0.75F, 0.5F)
+                                    )
+                            )
+                    )
+            ;
+
+
+        }
+
+        private boolean shouldShowForPlayer(Trigger trigger, Player player) {
+            return Interval.ALWAYS.equals(trigger.getRepeat())
+                    || playerTriggerActivationService.loadAll().stream()
+                    .noneMatch(playerTriggerActivation -> playerTriggerActivation.getPlayerId().equals(player.getUniqueId())
+                            && playerTriggerActivation.getTriggerId().equals(trigger.getId()));
         }
     }
 }
