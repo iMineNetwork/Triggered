b027dd084bc1842064a1599a15983586aa817c20
Sansko1337 <sanderknauff@hotmail.com>
2018-06-10 15:08:02 +0200

    Throw an exception when a move can not be found instead of failing silently

--- src/main/java/com/imine/pixelmon/json/AttackDeserializer.java
+++ src/main/java/com/imine/pixelmon/json/AttackDeserializer.java
@@ -5,22 +5,17 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.pixelmonmod.pixelmon.battles.attacks.Attack;
 import com.pixelmonmod.pixelmon.battles.attacks.AttackBase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class AttackDeserializer extends JsonDeserializer<Attack> {
 
-    private static final Logger logger = LoggerFactory.getLogger(AttackDeserializer.class);
-
     @Override
     public Attack deserialize(JsonParser parser, DeserializationContext context) throws IOException {
         String attackName = parser.getText();
-        return AttackBase.getAttackBase(attackName).map(Attack::new).orElseGet(() -> {
-            logger.warn("No attack found with name '{}' Context: (name: {}, line: {})", attackName, parser.getCurrentLocation().getLineNr(), parser.currentToken().name());
-            return null;
-        });
+        return AttackBase.getAttackBase(attackName).map(Attack::new).orElseThrow(() ->
+                new IllegalArgumentException("No attack found with name '" + attackName + "' Context: (name: " + parser.getCurrentLocation().getLineNr() + ", line: " + parser.currentToken().name() + ")")
+        );
 
     }
 }
